59c260b80cb9c62a06666e363499bba8db90ba63
Urmat Kushubekov <urmat.kushubekov@mail.ru>
2023-05-26 18:20:18 +0600

    создал клетку и полю

---
+++ src/components/playingField/playingField.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+
+
+const PlayingField = () => {
+    // let squares = [];
+    //
+    // for (let i = 1; i <= 36; i++) {
+    //     squares += <Square/>
+    // }
+
+    return (
+        <div className="container">
+
+        </div>
+    );
+};
+
+export default PlayingField;
